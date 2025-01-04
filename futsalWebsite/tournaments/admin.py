205dc1d76b8fb404e88b97ded7adf648284c26eb
callmedas <deepakkdas77@gmail.com>
2025-01-04 21:58:57 +0545

    models and admin.py is made

--- futsalWebsite/tournaments/admin.py
+++ futsalWebsite/tournaments/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
-
+from .models import Team, Player, Match
 # Register your models here.
+
+class PlayerInline(admin.TabularInline):
+    model = Player
+    extra = 1 
+
+
+# Team Admin
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'captain_name')
+    search_fields = ('name', 'captain_name')
+    inlines = [PlayerInline]
+
+# Player Admin
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number', 'team')
+    search_fields = ('name', 'team') 
+    list_filter = ('team',)
+
+# Match Admin
+@admin.register(Match)
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ('team1', 'team2', 'date', 'is_completed', 'is_penalty')
+
+    actions = ['start_penalty_shootout']
--- futsalWebsite/tournaments/models.py
+++ futsalWebsite/tournaments/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    captain_name = models.CharField(max_length=100)
+    captain_phone_number = models.CharField(max_length=15)
+    points = models.IntegerField(default=0) 
+
+    def __str__(self):
+        return self.name
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='players')
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Match(models.Model):
+    team1 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='home_matches')
+    team2 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='away_matches')
+    date = models.DateTimeField()
+    score_team1 = models.IntegerField(default=0)
+    score_team2 = models.IntegerField(default=0)
+    is_completed = models.BooleanField(default=False)
+    is_penalty = models.BooleanField(default=False)
+    penalty_team1 = models.JSONField(default=list)
+    penalty_team2 = models.JSONField(default=list)
